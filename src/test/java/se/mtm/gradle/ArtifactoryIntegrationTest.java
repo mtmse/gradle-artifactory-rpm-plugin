4b3091d71c2ceb3919a3ad5a7ea9115a84afde1a
Thomas Sundberg <thomas.sundberg@mtm.se>
2015-03-17 13:10:07 +0100

    Ignore the integration tests

--- src/test/java/se/mtm/gradle/ArtifactoryIntegrationTest.java
+++ src/test/java/se/mtm/gradle/ArtifactoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package se.mtm.gradle;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.mtm.gradle.infrastructure.*;
 
@@ -19,6 +20,7 @@ public class ArtifactoryIntegrationTest {
     }
 
     @Test
+    @Ignore
     public void check_that_no_artifact_is_uploaded_upload_an_artifact_see_that_it_is_uploaded_remove_it_and_see_that_it_is_gone() throws IOException {
         String buildDirectory = "./src/test/resources/build/distributions/";
         Artifact artifact = new Artifact(new File(buildDirectory + "rpm-to-artifactory-example-1.0.0-1.noarch.rpm"));
@@ -38,6 +40,7 @@ public class ArtifactoryIntegrationTest {
     }
 
     private void clearRepository() throws IOException {
+        assertTrue(false);
         RepositoryContent artifacts = FindRpms.in(repository, artifactoryHost);
         for (String artifact : artifacts.getFiles()) {
             PurgeRpm.purge(new Artifact(new File(artifact)), repository, artifactoryHost);
