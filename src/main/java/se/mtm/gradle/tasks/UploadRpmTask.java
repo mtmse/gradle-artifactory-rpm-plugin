46798a19ec69ee984c4d30490f318fc260f699f2
Thomas Sundberg <thomas.sundberg@mtm.se>
2015-03-17 13:02:59 +0100

    Enabled a proper implementation

--- src/main/java/se/mtm/gradle/tasks/UploadRpmTask.java
+++ src/main/java/se/mtm/gradle/tasks/UploadRpmTask.java
@@ -2,19 +2,32 @@ package se.mtm.gradle.tasks;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+import se.mtm.gradle.defaults.ArtifactoryRpmPluginDefaults;
 import se.mtm.gradle.infrastructure.Artifact;
+import se.mtm.gradle.infrastructure.UploadRpm;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class UploadRpmTask extends DefaultTask {
     @TaskAction
     public void uploadRpm() throws IOException {
-        String buildDirectory = "";
-        Artifact artifact = new Artifact("");
-        String repository = "mtm-utv";
-        String artifactoryHost = "http://artifactory.mtm.se:8081/artifactory";
+        ArtifactoryRpmPluginDefaults extension = getProject().getExtensions().findByType(ArtifactoryRpmPluginDefaults.class);
 
-        // todo enable
-        // UploadRpm.to(buildDirectory, artifact, repository, artifactoryHost);
+        if (extension == null) {
+            extension = new ArtifactoryRpmPluginDefaults();
+        }
+
+        String buildDirectory = extension.getBuildDirectory();
+        String repository = extension.getRepositoryName();
+        String artifactoryHost = extension.getRepositoryServerUrl();
+
+        List<File> rpms = UploadRpm.getAllRpms(buildDirectory);
+
+        for (File rpm : rpms) {
+            Artifact artifact = new Artifact(rpm);
+            UploadRpm.to(artifact, repository, artifactoryHost);
+        }
     }
 }
