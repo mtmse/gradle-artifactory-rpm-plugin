f494fe12bc85d5b906939eb33deb519ed20e3f7d
Thomas Sundberg <tsu@kth.se>
2015-11-11 09:04:32 +0100

    Call the promote tasks promoteToXXXRepo to indicate that they will only promote packages to a repo

--- src/main/java/se/mtm/gradle/ArtifactoryRpmPlugin.java
+++ src/main/java/se/mtm/gradle/ArtifactoryRpmPlugin.java
@@ -8,20 +8,14 @@ import se.mtm.gradle.tasks.PromoteToStageTask;
 import se.mtm.gradle.tasks.PromoteToTestTask;
 import se.mtm.gradle.tasks.PromoteToUtvTask;
 
-import javax.inject.Inject;
-
 class ArtifactoryRpmPlugin implements Plugin<Project> {
-    @Inject
-    public ArtifactoryRpmPlugin() {
-    }
-
     @Override
     public void apply(Project project) {
         project.getExtensions().create("artifactoryRpm", GradleArtifactoryRpmPluginDefaults.class);
 
-        project.getTasks().create("promoteToStage", PromoteToStageTask.class);
-        project.getTasks().create("promoteToUtv", PromoteToUtvTask.class);
-        project.getTasks().create("promoteToTest", PromoteToTestTask.class);
-        project.getTasks().create("promoteToProd", PromoteToProdTask.class);
+        project.getTasks().create("promoteToStageRepo", PromoteToStageTask.class);
+        project.getTasks().create("promoteToUtvRepo", PromoteToUtvTask.class);
+        project.getTasks().create("promoteToTestRepo", PromoteToTestTask.class);
+        project.getTasks().create("promoteToProdRepo", PromoteToProdTask.class);
     }
 }
